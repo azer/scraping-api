1325c38ee80dbc80f3ed25cf1ce5ce07d94c30ae
E.Azer Koçulu <azer@kodfabrik.com>
2014-04-29 01:12:32 -0700

    callback option

--- README.md
+++ README.md
@@ -44,5 +44,10 @@ You can get attribute values by choosing `attr:?` as node value in the query:
 $ curl -X POST -d '{"url":"http://azer.io", "query": { "first-article": { "selector": ".articles li:first-child a", "node":"attr:href" } }}' http://localhost:8080
 ```
 
+Optionally, results delivered to a callback URL by specifying the "callback" parameter:
+
+```
+$ curl -X POST -d '{"url":"http://azer.io", "callback":"http://localhost/save-results", "query": { "title": { "selector": "h1:first-child", "node":"text" } }}' http://localhost:8080
+```
 
 ![](http://distilleryimage5.ak.instagram.com/51eb9256ba2611e3a63112f56a54141d_6.jpg)
--- scraping-api.go
+++ scraping-api.go
@@ -3,7 +3,12 @@ package main
 import (
 	"github.com/azer/atlas"
 	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"net/url"
+	"encoding/json"
 	"strings"
+	"fmt"
+	. "github.com/azer/debug"
 )
 
 type Query struct {
@@ -13,6 +18,7 @@ type Query struct {
 
 type Options struct {
   URL string
+	Callback string
   Query map[string]Query
 }
 
@@ -41,11 +47,26 @@ func Scrape(request *atlas.Request) *atlas.Response {
 		return atlas.Error(500, err)
 	}
 
+	if len(opts.Callback) > 0 {
+		go Deliver(opts)
+		return atlas.Success("Results will be posted to " + opts.Callback)
+	}
+
+	result, err := Select(opts)
+
+	if err != nil {
+		return atlas.Error(500, err)
+	}
+
+	return atlas.Success(result)
+}
+
+func Select(opts *Options) (result Results, err error) {
 	var doc *goquery.Document
-	var result = make(Results)
+	result = make(Results)
 
 	if doc, err = goquery.NewDocument(opts.URL); err != nil {
-		return atlas.Error(500, err)
+		return nil, err
 	}
 
 	for key, query := range opts.Query {
@@ -58,12 +79,12 @@ func Scrape(request *atlas.Request) *atlas.Response {
 		}
 
 		if query.Node == "html" {
-			value, err = el.Html()
-				value = strings.Replace(value, "\u003c", "<", -1)
-				value = strings.Replace(value, "\u003e", ">", -1)
-				value = strings.Replace(value, "<br>", "\n", -1)
-				value = strings.Replace(value, "<br/>", "\n", -1)
-				value = strings.Replace(value, "<br />", "\n", -1)
+			value, _ := el.Html()
+			value = strings.Replace(value, "\u003c", "<", -1)
+			value = strings.Replace(value, "\u003e", ">", -1)
+			value = strings.Replace(value, "<br>", "\n", -1)
+			value = strings.Replace(value, "<br/>", "\n", -1)
+			value = strings.Replace(value, "<br />", "\n", -1)
 		}
 
 		if len(query.Node) > 5 && query.Node[0:5] == "attr:" {
@@ -78,5 +99,46 @@ func Scrape(request *atlas.Request) *atlas.Response {
 		}
 	}
 
-	return atlas.Success(result)
+	return result, nil
+}
+
+func DeliverError(opts *Options, err string) {
+	Debug("Request to %s failed. Error: %s", opts.Callback, err)
+
+	data := url.Values{}
+	data.Set(fmt.Sprintf("{ \"error\": \"%s\" }", err), "")
+	_, derr := http.PostForm(opts.Callback, data)
+
+	if derr != nil {
+		Debug("Failed to post the error message '%s' to %s", err, opts.URL)
+	}
+}
+
+func Deliver(opts *Options) {
+	Debug("Results of %s will be delivered to %s", opts.URL, opts.Callback)
+
+	result, err := Select(opts)
+
+	data := url.Values{}
+
+	if err != nil {
+		DeliverError(opts, "Failed to parse and extract the data.")
+		return
+	}
+
+	strResult, err := json.Marshal(result)
+
+	if err != nil {
+		DeliverError(opts, "Apologizes, scraping-api screwed up.")
+		return
+	}
+
+	data.Set(string(strResult), "")
+
+	Debug("Posting results to %s", opts.Callback)
+	_, err = http.PostForm(opts.Callback, data)
+
+	if err != nil {
+		Debug("Unable to post to %s", opts.Callback)
+	}
 }
